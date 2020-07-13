da8c68be4dda22c681d3e12dd84bc9a765a00c45
Quang Bui <buiphuquang2412@gmail.com>
2020-07-13 16:39:52 +0300

    increase size of country picker

--- src/component/CountryPicker/CountryPicker.js
+++ src/component/CountryPicker/CountryPicker.js
@@ -16,7 +16,7 @@ const CountryPicker = ({ countryChangeHandler }) => {
 
     return (
         <div className={styles.formControl}>
-            <FormControl>
+            <FormControl fullWidth>
                 <NativeSelect defaultValue="" onChange={e => countryChangeHandler(e.target.value)}>
                     <option value="">Global</option>
                     {countries.map(country => <option key={country} value={country}>{country}</option>)}
